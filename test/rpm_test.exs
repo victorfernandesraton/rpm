b47a338ac3f49f00229591a64bb1ae60b67115b0
victorfernandesraton <vfbrato@gmail.com>
2022-12-16 11:47:21 -0300

    feat: identing test

--- test/rpm_test.exs
+++ test/rpm_test.exs
@@ -2,16 +2,20 @@ defmodule RpmTest do
   use ExUnit.Case
   doctest Rpm
 
-  test "['2','1','+','3','*'] should be 9" do
+  test """
+    ["2", "1", "+", "3", "*"] should be 9
+  """ do
     assert Rpm.eval_rpn(["2", "1", "+", "3", "*"]) == 9
   end
 
-  test "['4','13','5','/','+'] should be 6" do
+  test """
+    ["4", "13", "5", "/", "+"] should be 6
+  """ do
     assert Rpm.eval_rpn(["4", "13", "5", "/", "+"]) == 6
   end
 
   test """
-  ["10","6","9","3","+","-11","*","/","*","17","+","5","+"] should be 22
+    ["10","6","9","3","+","-11","*","/","*","17","+","5","+"] should be 22
   """ do
     assert Rpm.eval_rpn(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]) ==
              22
